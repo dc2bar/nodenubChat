a160a503ab66d9613c4d3abdb7702ddd98f175cf
dc2bar <dc2bar@gmail.com>
2014-02-17 17:53:47 -0700

    handlebars template try 1

--- public/js/views/views.js
+++ public/js/views/views.js
@@ -18,26 +18,129 @@ $(function () {
   /*------------Views----------------*/
 
   app.LoginView = Backbone.View.extend({
-    el: '.login-view',
-    template: 'templates/login',
+    el: '.body_wrap',
+    template: 'login',
+    usernameField: '#username',
+    passwordField: '#password',
+    avatarField: '#image',
+    colorField: '#color',
+    avatarURL: 'images/avatar_placeholder.jpg',
+    chatColor: '#959da6',
     model: app.users,
     events: {
-      'keyup #username': 'validateUsername',
-      'click .remove-provider': 'removeProvider'
+      'click #login': 'login',
+      'click #image': 'editAvatar',
+      'click #color': 'editColor'
     },
     initialize: function () {
       this.model.bind('reset', this.render, this);
+      this.render();
     },
-    render: function (e) {
+    render: function () {
       var thisView = this;
-      var template = HandlebarsTemplates[this.template]({providers: this.model.toJSON()});
+      var template = Handlebars.templates[this.template]({providers: this.model.toJSON()});
       thisView.$el.html(template);
       this.delegateEvents();
     },
-    validateUsername: function (e) {
-      console.log(e);
+    login: function () {
+      var username = $(this.usernameField, this.el).val();
+      var password = $(this.passwordField, this.el).val();
+      this.validateUsername(username);
+      this.validatePassword(password);
+      this.validateAvatar();
+      this.validateColor();
+    },
+    editAvatar: function () {
+      var avatarURL = prompt('Enter the URL for your avatar',this.avatarURL);
+      this.avatarURL = avatarURL;
+      $(this.avatarField + ' img', this.el).attr('src',this.avatarURL);
+    },
+    editColor: function () {
+      var chatColor = prompt('Enter a hexadecimal chat color.',this.chatColor);
+      this.chatColor = chatColor;
+      $(this.colorField, this.el).css('background-color',this.chatColor);
+    },
+    validateUsername: function (username) {
+      console.log(username)
+    },
+    validatePassword: function (password) {
+      console.log(password);
+    },
+    validateColor: function () {
+      console.log(this.chatColor);
+    },
+    validateAvatar: function () {
+      console.log(this.avatarURL);
     }
   });
+
+  app.ChatView = Backbone.View.extend({
+    el: '.body_wrap',
+    template: 'login',
+    usernameField: '#username',
+    passwordField: '#password',
+    avatarField: '#image',
+    colorField: '#color',
+    avatarURL: 'images/avatar_placeholder.jpg',
+    chatColor: '#959da6',
+    model: app.users,
+    events: {
+      'click #login': 'login',
+      'click #image': 'editAvatar',
+      'click #color': 'editColor'
+    },
+    initialize: function () {
+      this.model.bind('reset', this.render, this);
+      this.render();
+    },
+    render: function () {
+      var thisView = this;
+      var template = Handlebars.templates[this.template]({providers: this.model.toJSON()});
+      thisView.$el.html(template);
+      this.delegateEvents();
+      this.ChatMessagesView.setElement(this.$('.chat-messages')).render();
+      this.UsersListView.setElement(this.$('.userslist')).render();
+      return this;
+    }
+  });
+
+  app.ChatMessagesView = Backbone.View.extend({
+    el: '.chat-messages',
+    template: 'chatpanel',
+    model: app.users,
+    events: {
+      'click .widget_profile': 'launchUserOptions'
+    },
+    initialize: function () {
+      this.model.bind('reset', this.render, this);
+      this.render();
+    },
+    render: function () {
+      var thisView = this;
+      var template = Handlebars.templates[this.template]({providers: this.model.toJSON()});
+      thisView.$el.html(template);
+      this.delegateEvents();
+    }
+  })
+
+  app.UsersListView = Backbone.View.extend({
+    el: '.userslist',
+    template: 'userslist',
+    model: app.users,
+    events: {
+      'click .widget_profile': 'launchUserOptions'
+    },
+    initialize: function () {
+      this.model.bind('reset', this.render, this);
+      this.render();
+    },
+    render: function () {
+      var thisView = this;
+      var template = Handlebars.templates[this.template]({providers: this.model.toJSON()});
+      thisView.$el.html(template);
+      this.delegateEvents();
+    }
+  })
 
   new app.start();
 });
